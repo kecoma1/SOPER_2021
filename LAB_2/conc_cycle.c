8835d728fbd1c85e7cd98d83f6555d747a1a1e21
kecoma1 <kecoma2@gmail.com>
2021-03-21 13:41:11 +0100

    casi
    Co-authored-by: Marcos Bernuy <mbernuy@users.noreply.github.com>

--- LAB_2/conc_cycle.c
+++ LAB_2/conc_cycle.c
@@ -24,6 +24,9 @@
 /* Funciones básicas para la ejecución */
 #include <stdlib.h> 
 
+/* Para hacer conversión a intmax_t */
+#include <stdint.h>
+
 /* Uso de fork, getpid */
 #include <sys/types.h>
 #include <unistd.h>
@@ -37,10 +40,10 @@
 /* Espera a los hijos */
 #include <sys/wait.h>
 
-/* Variables globales para que el 
-manejador pueda acceder a estas */
-int ciclo = 0;
-pid_t pid_hijo = 0, pid = 0;
+/* Flag para determinar si se han recibido diferentes señales */
+short sigusr1_received = 0;
+short sigterm_received = 0;
+short sigint_received = 0;
 
 
 /**
@@ -49,75 +52,69 @@ pid_t pid_hijo = 0, pid = 0;
  * @param sig Señal recibida
  */
 void manejador_SIGUSR(int sig) {
-    /* Sleep para que sea "visible" la ejecución */
-    sleep(1);
-
-    if (pid_hijo == 0) {
-        /* Enviamos SIGUSR1 al padre de todos */
-        if (kill(pid, SIGUSR1) == -1) {
-            perror("kill");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        /* Enviamos SIGUSR1 al hijo */
-        if (kill(pid_hijo, SIGUSR1) == -1) {
-            perror("kill");
-            exit(EXIT_FAILURE);
-        }
-    }
-    ciclo++;
-    printf("Ciclo: %d - PID: %d\n", ciclo, getpid());
+    printf("hola_ sigusr1\n");
+    sigusr1_received = 1;
 }
 
 
 /**
- * @brief Manejador para que cuando el padre reciba
- * SIGINT, este envié a su hijo SIGTERM, y el hijo
- * a su hijo...
+ * @brief Manejador para que marcar que se ha recibido
+ * la señal SIGINT.
  * 
  * @param sig Señal recibida
  */
 void manejador_SIGINT(int sig) {
-    /* Enviamos SIGTERM al hijo */
-    if (kill(pid_hijo, SIGTERM) == -1) perror("kill");
-
-    printf("Padre abandona a sus hijos.\n");
-
-    /* Esperamos al hijo */
-    waitpid(pid_hijo, NULL, WEXITED);
-    exit(EXIT_SUCCESS);
+    printf("hola_ sigint\n");
+    sigint_received = 1;
 }
+
 
 /**
  * @brief Manejador para la señal SIGTERM.
- * Cada vez que un proceso recibe la señal,
- * este libera recursos y termina su ejecución
- * aquí.
  * 
  * @param sig Señal recibida 
  */
 void manejador_SIGTERM(int sig) {
-    printf("PID %d termina su ejecución.\n", getpid());
-    fflush(stdin);
+    printf("hola_ sigterm\n");
+    sigterm_received = 1;
+}
 
-    /* Enviamos SIGTERM al hijo (si tiene) */
-    if (pid_hijo != 0) {
-        if (kill(pid_hijo, SIGTERM) == -1) perror("kill");
+
+/**
+ * @brief Función para enviar al hijo la señal
+ * 
+ * @param hijo PID del hijo
+ * @param pid_P1 PID del primer proceso
+ * @param signal Señal a enviar
+ * @return int  0 OK, -1 ERR
+ */
+int send_signal(int hijo, int pid_P1, int signal) {
+    if (hijo != 0) { // 
+        /* Enviamos a nuestro hijo la señal SIGUSR1 */
+        if (kill(hijo, signal) == -1) {
+            perror("kill");
+            return -1;
+        }
+    } else {
+        /* Si no tenemos hijo, se lo enviamos a P1 */
+        if (kill(pid_P1, signal) == -1) {
+            perror("kill");
+            return -1;
+        }
     }
-
-    /* Esperamos a nuestro hijo */
-    waitpid(pid_hijo, NULL, WEXITED);
-    exit(EXIT_SUCCESS);
+    return 0;
 }
 
 
 int main(int args, char* argv[]) {
-    int num_proc = 0, i = 0;
+    int num_proc = 0, ciclo = 1;
+    pid_t pid_hijo = 0, pid_P1 = 0;
+    sigset_t hijo_mask, old_set;
 
     /* Estructuras para capturar señales */
     struct sigaction act_SIGUSR1;
-    struct sigaction act_SIGTERM;
     struct sigaction act_SIGINT;
+    struct sigaction act_SIGTERM;
 
     if (args < 2) {
         printf("Introduzca el número de procesos.\n./conc_cycle <número de procesos>\n");
@@ -127,61 +124,97 @@ int main(int args, char* argv[]) {
         num_proc = atoi(argv[1]);
     }
 
-    /* Creamos los manjeadores */
+    /* Creamos los manejadores */
     act_SIGUSR1.sa_handler = manejador_SIGUSR;
-    act_SIGTERM.sa_handler = manejador_SIGTERM;
     act_SIGINT.sa_handler = manejador_SIGINT;
+    act_SIGTERM.sa_handler = manejador_SIGUSR;
 
+    /* Inicializando las mascaras */
     sigemptyset(&(act_SIGUSR1.sa_mask));
-    sigemptyset(&(act_SIGTERM.sa_mask));
     sigemptyset(&(act_SIGINT.sa_mask));
+    sigemptyset(&(act_SIGTERM.sa_mask));
 
     act_SIGUSR1.sa_flags = 0;
-    act_SIGTERM.sa_flags = 0;
     act_SIGINT.sa_flags = 0;
+    act_SIGTERM.sa_flags = 0;
 
-    /* Obtenemos el pid del padre */
-    pid = getpid();
+    /* Mascaras para el bloqueo de señales */
+    sigemptyset(&hijo_mask);
+    sigaddset(&hijo_mask, SIGINT);
 
-    /* Arbol de procesos */
-    for(i = 0; i < num_proc-1; i++) {
+    pid_P1 = getpid();
+
+    /* Bucle para crear procesos */
+    for(int i = 0; i < num_proc-1; i++){
         pid_hijo = fork();
-        
-        /* Ejecución del padre, sale del loop */
-        if (pid_hijo != 0) break;
+        if(pid_hijo != 0) break;
     }
 
-    /* "linkeamos" las señales con los manejadores */
-    if (sigaction(SIGUSR1, &act_SIGUSR1, NULL) < 0) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    if (sigaction(SIGTERM, &act_SIGTERM, NULL) < 0) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
+    /* Linkeamos la estructura sigaction a los procesos hijos */
+    if(pid_P1 != getpid()){
+        if(sigaction(SIGUSR1, &act_SIGUSR1, NULL) < 0) {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
 
-    /* "Linkeamos" el manejador de SIGINT al padre */
-    if (pid == getpid()) {
-        if (sigaction(SIGINT, &act_SIGINT, NULL) < 0) {
+        if(sigaction(SIGTERM, &act_SIGTERM, NULL) < 0) {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
+
+        /* Linkeamos la mascara para bloquear señales */
+        if (sigprocmask(SIG_BLOCK, &hijo_mask, &old_set) == -1) {
+            perror("sigprocmask");
+            exit(EXIT_FAILURE);
+        }
+    }
+    /* Linkeamos la estructura sigaction al proceso antecesor */
+    else {
+        if(sigaction(SIGUSR1, &act_SIGUSR1, NULL) < 0) {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
+
+        if(sigaction(SIGINT, &act_SIGINT, NULL) < 0) {
             perror("sigaction");
             exit(EXIT_FAILURE);
         }
     }
 
-    /* Dromimos al proceso para que espere a que sus hijos se creen */
-    sleep(1);
-
-    /* Si somos el padre iniciamos el ciclo */
-    if (pid == getpid()) {
-        if (kill(pid_hijo, SIGUSR1) == -1) {
-            perror("kill");
-            exit(EXIT_FAILURE);
-        }
+    /* El padre inicia el ciclo */
+    if (pid_P1 == getpid()) {
+        sleep(1);
+        send_signal(pid_hijo, pid_P1, SIGUSR1);
+        printf("Número de ciclo: %d, PID hijo: %jd, PID: %jd\n", ciclo, (intmax_t)pid_hijo, (intmax_t)getpid());
+        ciclo++;
     }
 
-    /* Bucle para enviar señales */
-    while (1) {
+    while(1) {
+        /* Espera inactiva de la señal */
         pause();
+        sleep(1);
+        if (sigusr1_received == 1) {
+            send_signal(pid_hijo, pid_P1, SIGUSR1);
+            printf("Número de ciclo: %d, PID hijo: %jd, PID: %jd\n", ciclo, (intmax_t)pid_hijo, (intmax_t)getpid());
+            ciclo++;
+            sigusr1_received = 0;
+        }
+        else if (sigint_received == 1){
+            printf("SIGINT recibido, acabando con la ejecución del ciclo. PID: %jd\n", (intmax_t)getpid());
+            /* Si el padre recibe la señal SIGINT, envía la señal SIGTERM a los hijos*/
+            send_signal(pid_hijo, pid_P1, SIGTERM);
+            printf("SIGINT recibido, acabando con la ejecución del ciclo. PID: %jd\n", (intmax_t)getpid());
+            waitpid(pid_hijo, NULL, WEXITED);
+            exit(EXIT_SUCCESS);
+        } else if (sigterm_received == 1) {
+            /* Enviamos la señal SIGTERM a nuestro hijo,
+            si no tiene hijo, no envía */
+            if (pid_hijo != 0) send_signal(pid_hijo, pid_P1, SIGTERM);
+            printf("Acabando con la ejecución de PID: %jd\n", (intmax_t)getpid());
+
+            /* Cada hijo espera a su hijo excepto el último hijo */
+            if (pid_hijo != 0) waitpid(pid_hijo, NULL, WEXITED);
+            exit(EXIT_SUCCESS);
+        }
     }
 }
